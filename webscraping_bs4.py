2e3b27209788353b4f7e5520cfd90365e1118f26
juishah14 <59267651+juishah14@users.noreply.github.com>
2021-01-03 14:55:01 -0500

    Add files via upload

---
+++ webscraping_bs4.py
@@ -0,0 +1,28 @@
+# Assignment from Coursera's Accessing Web Data using Python
+# For this assignment, we must write a Python program to use urllib and Beautiful Soup to parse HTML, 
+# scrape/extract numbers from the file, and compute their sum.
+# Test with http://py4e-data.dr-chuck.net/comments_564445.html. Answer should be 2552.
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+# returns entire document at that web page
+html = urlopen(url, context=ctx).read()
+# creating soup object
+soup = BeautifulSoup(html, "html.parser")
+
+sum = 0
+
+# Retrieve all of the span tags
+tags = soup('span')
+for tag in tags:
+    sum += int(tag.contents[0])
+    
+print(sum)
